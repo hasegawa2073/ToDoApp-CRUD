962b735595c2ac8ff65f273d9658157e7a38a5d4
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-12-11 22:25:08 +0900

    chore: メディアクエリ、カスタム(max-widthで)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "-2xl": { max: "1535px" },
+        "-xl": { max: "1279px" },
+        "-lg": { max: "1023px" },
+        "-md": { max: "767px" },
+        "-sm": { max: "639px" },
+      },
+    },
   },
   plugins: [],
 };
