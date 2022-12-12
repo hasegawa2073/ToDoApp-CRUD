0d79038b9b7b46f4e9d0f6580ce7a8d6ccdc8a17
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-12-12 19:25:50 +0900

    fix: タスク追加ページに遷移

--- src/components/CreateTaskButton.tsx
+++ src/components/CreateTaskButton.tsx
@@ -1,15 +1,18 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 const CreateTaskButton = () => {
   return (
     <>
-      <div className="inline-block cursor-pointer rounded-md bg-indigo-700 px-5 py-4 text-xl font-bold text-white transition-colors hover:bg-indigo-800">
-        <div className="flex items-center">
-          <FontAwesomeIcon icon={faPlus} className="text-2xl" />
-          <span className="pl-3">Create a new task</span>
+      <Link href="/add">
+        <div className="hover:bg-indigo-00 inline-block cursor-pointer rounded-md bg-indigo-700 px-5 py-4 text-xl font-bold text-white transition-colors">
+          <div className="flex items-center">
+            <FontAwesomeIcon icon={faPlus} className="text-2xl" />
+            <span className="pl-3">Create a new task</span>
+          </div>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
